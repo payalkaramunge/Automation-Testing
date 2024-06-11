74dcf84edffec5605aadcdfb2fa260eb10a70e8d
payalkaramunge <karamungepayal@gmail.com>
2024-06-11 10:36:34 +0530

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/41/001cd2dd0c27001f1639bc799684e2f5
@@ -0,0 +1,5 @@
+package Day_2_java;
+
+public class FirstJavaProgram {
+
+}
---
+++ java_Programming/src/Day_2_java/SecondJavaProgram.java
@@ -0,0 +1,13 @@
+
+//		Data Types
+
+package Day_2_java;
+
+public class SecondJavaProgram {
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
